356330c231b380b94c4f80f63e1ba73d0692204d
Your Name <you@example.com>
2026-04-20 03:47:26 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -148,7 +148,18 @@ Pair * nextMap(HashMap * map) {
 
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
-
+    Pair** old_buckets = map->buckets;
+    long old_capacity = map->capacity;
+    map->capacity *= 2;
+    map->buckets = (Pair**) malloc(map->capacity * sizeof(Pair*));
+    for (int i = 0; i < map->capacity; i++) map->buckets[i] = NULL;
+    map->size = 0;
+    for (int i = 0; i < old_capacity; i++) {
+        if (old_buckets[i] != NULL && old_buckets[i]->key != NULL) {
+            insertMap(map, old_buckets[i]->key, old_buckets[i]->value);
+        }
+    }
+    free(old_buckets);
 
 }
 
